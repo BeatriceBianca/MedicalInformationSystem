914398529f7007fd933634b05db9a738b4ab1425
Beatrice Bianca <bianca.luca@mindit.ro>
2019-01-08 19:20:28 +0200

    First commit

---
+++ src/com/company/Pacient.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.List;
+
+public class Pacient {
+
+    private String lastName;
+    private String firstName;
+    private int age;
+    private List<String> disease;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getDisease() {
+        return disease;
+    }
+
+    public void setDisease(List<String> disease) {
+        this.disease = disease;
+    }
+}
